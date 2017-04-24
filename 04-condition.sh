cadb52b453f57e11f176e02fe2dbd2ff26f4ad78
Luc Labelle <luc.labelle@gmail.com>
2017-04-24 17:55:50 -0400

    Done with conditions

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then
+	echo "you failed!"
+elif test $note -eq 60
+then
+	echo "ouf! c'est juste!"
+else
+	echo "Alright"
+fi
 
+read -p "Entrez une lettre:" $lettre
 
+case $lettre in
+	[[:lower:]])
+		echo "lettre en minuscule"
+		;;
+	*)
+		echo "Majuscule"
+		;;
+esac
 
-
+if test $lettre != "a"
+then
+	echo "la lettre n'est pas un a"
+fi
 
 
