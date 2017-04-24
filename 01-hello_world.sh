2bd5e0d419ec1690776e0d84ecaf24dea4566e62
Luc Labelle <luc.labelle@gmail.com>
2017-04-24 17:33:49 -0400

    DOne with hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+echo "salut"
+#exit
+echo "toi"
+
+# -n = Prévenir un retour de ligne automatique
+echo -n "Vive"
+echo " Linux"
+
+# -e = Accepter les méta-caractères (\n, \t, ...)
+echo -e "1\n2\n3"
+
+#$ = lorsque l'on veut la valeur de la variable (lecture)
+# sinon, on affecte dans la variable (sans le $)
+read -p "Quel age avez-vous?" age
+echo $age "ans! Vous paraissez jeune!"
