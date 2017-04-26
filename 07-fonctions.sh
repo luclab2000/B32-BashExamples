64ee38d43e2adcce47a4fd331ea20db03bfda20a
Luc Labelle <luc.labelle@gmail.com>
2017-04-26 15:11:08 -0400

    On peut faire des exp. régulières

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+maFonction() {
+	echo "salut $1"
+}
+
+maFonction Marc0
+maFonction JS
+maFonction JU
--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+# Identique à read -p "Lettre: " lettre
+echo -n "Lettres: "
+read lettres
+
+if [[ $lettres =~  ^[0-9]+$  ]]
+then
+	echo "C'est un nombre"
+fi
