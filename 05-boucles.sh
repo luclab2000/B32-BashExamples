9b4acffa1add1d02dbcc4f500ca203897d04ac5b
Luc Labelle <luc.labelle@gmail.com>
2017-04-26 14:55:53 -0400

    3 sortes de boucles!

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+
+	echo $i
+done
+
+resultat=o
+
+while test $resultat = "o"
+do
+	read -p "Entrez une lettre : " resultat
+done
+
+maListe=`ls`
+
+for element in $maListe
+do
+	echo $element
+done
